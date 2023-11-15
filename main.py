815ad9054e2c6bf8cdfef7f5944092f024351594
kittiposete <96778671+kittiposete@users.noreply.github.com>
2023-11-15 09:35:52 +0700

    update

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import iq180_pb2_grpc
 import iq180_pb2
 
 # connect to server
-host_ip = '192.168.1.127'
-channel = grpc.insecure_channel(host_ip + ':8888')
+host_ip = 'eteownserver.thddns.net'
+channel = grpc.insecure_channel(host_ip + ':3882')
 stub = iq180_pb2_grpc.IQ180Stub(channel)
 print("please enter your name:  ", end="")
 player_name = input()
